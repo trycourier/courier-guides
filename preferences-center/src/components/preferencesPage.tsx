0d1733614466e250131767274f08405071ba8079
Nathalia Sandoval <nathaliam.sandoval@gmail.com>
2023-01-23 11:02:54 -0800

    update .gitignore

--- preferences-center/src/components/preferencesPage.tsx
+++ preferences-center/src/components/preferencesPage.tsx
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
-import { CourierProvider } from "@trycourier/react-provider";
-import { PreferenceList } from "@trycourier/react-preferences";
+import {CourierProvider} from '@trycourier/react-provider';
+import {PreferenceList} from '@trycourier/react-preferences';
 import Divider from '@mui/material/Divider';
 import UserNameModal from './usernameModal';
 const PreferencesPage: React.FunctionComponent = () => {
     const [userName, setUserName] = useState<string>('');
     const [showUserNameModal, setShowUserNameModal] = useState<boolean>(true);
-    const clientKey = process.env.REACT_APP_COURIER_CLIENT_KEY
+    const clientKey = process.env.REACT_APP_COURIER_CLIENT_KEY;
     return (
         <div className="preferences-page-container">
             <UserNameModal
@@ -16,18 +16,18 @@ const PreferencesPage: React.FunctionComponent = () => {
                 userName={userName}
             />
 
-            {!showUserNameModal && clientKey &&(
-              
+            {!showUserNameModal && clientKey && (
                 <div className="preferences-center-courier-embed">
-                            <h1 className="user-greeting">Hello, {userName}</h1>
-               <p>Configure your newsletter communications below</p>
-               <Divider/>
-               <div className='preferences-center-courier-embed-list'><CourierProvider
-      clientKey={clientKey}
-      userId={userName}>
-      <PreferenceList />
-    </CourierProvider></div>
-                    
+                    <h1 className="user-greeting">Hello, {userName}</h1>
+                    <p>Configure your newsletter communications below</p>
+                    <Divider />
+                    <div className="preferences-center-courier-embed-list">
+                        <CourierProvider
+                            clientKey={clientKey}
+                            userId={userName}>
+                            <PreferenceList />
+                        </CourierProvider>
+                    </div>
                 </div>
             )}
         </div>
